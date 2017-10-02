dfbf152060e5e870e15477495623bb91fd45e211
Marat Agliulin <marat@net107.186.188-156.tmn.ertelecom.ru>
2017-10-02 23:09:58 +0500

    auth

--- index.js
+++ index.js
@@ -52,6 +52,7 @@ let transporter = nodemailer.createTransport({
 app.set('port', (process.env.PORT || 5000));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+app.engine('pug', require('pug').__express)
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
