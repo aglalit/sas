b56ff4202e024629b25274677a14a2d06e5e1dfd
Marat Agliulin <marat@net61.186.188-143.tmn.ertelecom.ru>
2017-10-03 21:52:20 +0500

    auth

--- index.js
+++ index.js
@@ -111,9 +111,13 @@ function isLoggedIn(req, res, next) {
 
   // if user is authenticated in the session, carry on
   if (req.isAuthenticated())
-    return next();
+  {
+    console.log(req.isAuthenticated());
+    console.log('isAuthenticated');
+    return next();}
 
   // if they aren't redirect them to the home page
+  console.log(req.isAuthenticated());
   res.redirect('/login');
 }
 
