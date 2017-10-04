1c0dc52b22d316cc1a66d153e3a4391f44913e5a
Marat Agliulin <marat@240-53-1-5.rev.ruscomp.net>
2017-10-04 13:22:52 +0500

    auth

--- models/user.js
+++ models/user.js
@@ -12,18 +12,12 @@ var userSchema = mongoose.Schema({
   },
   polls: {
     AI_Metaphor: {
-      LECTURES: {
-        question1: {
-          label: "Как хорошо были организованы материалы курса (ридер, раздатки, книги, электронные копии книг, аудиовизуальные материалы, e-mail рассылки, соцсети, web-сайт и т.д.).Оцените по шкале от 0 до 10, где 0 – материалов вообще не было или ими было невозможно пользоваться; 5 – материалов явно не хватало или пользоваться ими было затруднительно; 10 – материалов было совершенно достаточно, они были легко доступны и прекрасно структурированы.
- ",
-        value: Number
-      },
-      question2: {
-        label: "Как хорошо были организованы материалы курса (ридер, раздатки, книги, электронные копии книг, аудиовизуальные материалы, e-mail рассылки, соцсети, web-сайт и т.д.).Оцените по шкале от 0 до 10, где 0 – материалов вообще не было или ими было невозможно пользоваться; 5 – материалов явно не хватало или пользоваться ими было затруднительно; 10 – материалов было совершенно достаточно, они были легко доступны и прекрасно структурированы.",
-      value: Number
+      LECTURES:{
+        "Как хорошо были организованы материалы курса" : Number
       }
+      SEMINARS:{
+
       }
-      SEMINARS: {}
     }
   }
 
@@ -40,7 +34,7 @@ userSchema.methods.validPassword = function(password) {
   return bcrypt.compareSync(password, this.local.password);
 };
 
-userSchema.methods.poll = function(req) {
+userSchema.methods.poll = function(req){
   User.findOne({
     '_id': req.user._id
   }, function(err, user) {
@@ -48,8 +42,8 @@ userSchema.methods.poll = function(req) {
       return done(err);
 
     if (user) {
-      user.polls.AI_Metaphor.LECTURES.question1 = req.body.question1;
-      console.log(req.body);
+      user.polls.AI_Metaphor.LECTURES["Как хорошо были организованы материалы курса"] = req.body["Как хорошо были организованы материалы курса"];
+console.log(req.body);
       user.save(function(err, user) {
         if (err)
           return console.error(err);
