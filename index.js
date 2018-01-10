565fce266ec81b074c9c47b2cd495d5fbe15d309
Marat Agliulin <marat@Marats-MacBook-Pro.local>
2018-01-10 14:15:38 +0500

    fsdf

--- index.js
+++ index.js
@@ -959,6 +959,101 @@ app.post('/polls/22-23-lectures', function(req, res) {
 
     if (session) {
       var now = new Date();
+      session.polls.electives__17__2["Укажите имя и фамилию преподавателя, работавшего с вами на семинарах"] = req.body["Укажите имя и фамилию преподавателя, работавшего с вами на семинарах"];
+
+      session.polls.electives__17__2["Как хорошо были организованы материалы курса"] = parseInt(req.body["Как хорошо были организованы материалы курса"]);
+      session.polls.electives__17__2["Сколько процентов обязательных текстов по этой части курса вы успевали прочесть"] = parseInt(req.body["Сколько процентов обязательных текстов по этой части курса вы успевали прочесть"]);
+      session.polls.electives__17__2["Насколько полно преподаватель следовал программе курса"] = parseInt(req.body["Насколько полно преподаватель следовал программе курса"]);
+      session.polls.electives__17__2["Оцените сложность лекций для вашего понимания"] = parseInt(req.body["Оцените сложность лекций для вашего понимания"]);
+      session.polls.electives__17__2["Насколько новым для вас было содержание лекций"] = parseInt(req.body["Насколько новым для вас было содержание лекций"]);
+      session.polls.electives__17__2["Насколько пунктуален был преподаватель"] = parseInt(req.body["Насколько пунктуален был преподаватель"]);
+      session.polls.electives__17__2["Насколько изменились ваше мышление, знания и умения"] = parseInt(req.body["Насколько изменились ваше мышление, знания и умения"]);
+      session.polls.electives__17__2["Хотите ли вы снова встретиться с этим преподавателем на других курсах"] = parseInt(req.body["Хотите ли вы снова встретиться с этим преподавателем на других курсах"]);
+      session.polls.electives__17__2["Насколько хорошо преподаватель объясняет материал"] = parseInt(req.body["Насколько хорошо преподаватель объясняет материал"]);
+      session.polls.electives__17__2["Насколько хорошо преподаватель организует интерактивную коммуникацию в группе"] = parseInt(req.body["Насколько хорошо преподаватель организует интерактивную коммуникацию в группе"]);
+      session.polls.electives__17__2["Что вам больше всего понравилось в этом лекционном курсе"] = req.body["Что вам больше всего понравилось в этом лекционном курсе"];
+      session.polls.electives__17__2["Что вам больше всего не понравилось в этом лекционном курсе"] = req.body["Что вам больше всего не понравилось в этом лекционном курсе"];
+      session.polls.electives__17__2["Что для вас было самым сложным в этом лекционном курсе"] = req.body["Что для вас было самым сложным в этом лекционном курсе"];
+      session.polls.electives__17__2["Как бы вы порекомендовали улучшить этот лекционный курс"] = req.body["Как бы вы порекомендовали улучшить этот лекционный курс"];
+      session.save(function(err, session) {
+        if (err)
+          return console.error(err);
+        }
+      );
+      let mailOptions = {
+        from: '"SAS" <sas@utmn.ru>', // sender address
+        to: 'marat.goya@gmail.com, e.samokhvalova@utmn.ru, a.rusakova@utmn.ru, e.selikhovkina@utmn.ru', // list of receivers
+        subject: 'Новый результат опроса по семинарам и лекциям Школы', // Subject line
+        // text: JSON.stringify(req.user), // plain text body
+        html: '<b>' + JSON.stringify(req.session.id) + '<br/><br/>' + JSON.stringify(req.body) + '</b>' // html body
+      };
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          return console.log(error);
+        }
+        console.log('Message %s sent: %s', info.messageId, info.response);
+      });
+    } else {
+      var newSession = new Session();
+      var now = new Date();
+      newSession.polls.electives__17__2["Укажите имя и фамилию преподавателя, работавшего с вами на семинарах"] = req.body["Укажите имя и фамилию преподавателя, работавшего с вами на семинарах"];
+
+      newSession.polls.electives__17__2["Как хорошо были организованы материалы курса"] = parseInt(req.body["Как хорошо были организованы материалы курса"]);
+      newSession.polls.electives__17__2["Сколько процентов обязательных текстов по этой части курса вы успевали прочесть"] = parseInt(req.body["Сколько процентов обязательных текстов по этой части курса вы успевали прочесть"]);
+      newSession.polls.electives__17__2["Насколько полно преподаватель следовал программе курса"] = parseInt(req.body["Насколько полно преподаватель следовал программе курса"]);
+      newSession.polls.electives__17__2["Оцените сложность лекций для вашего понимания"] = parseInt(req.body["Оцените сложность лекций для вашего понимания"]);
+      newSession.polls.electives__17__2["Насколько новым для вас было содержание лекций"] = parseInt(req.body["Насколько новым для вас было содержание лекций"]);
+      newSession.polls.electives__17__2["Насколько пунктуален был преподаватель"] = parseInt(req.body["Насколько пунктуален был преподаватель"]);
+      newSession.polls.electives__17__2["Насколько изменились ваше мышление, знания и умения"] = parseInt(req.body["Насколько изменились ваше мышление, знания и умения"]);
+      newSession.polls.electives__17__2["Хотите ли вы снова встретиться с этим преподавателем на других курсах"] = parseInt(req.body["Хотите ли вы снова встретиться с этим преподавателем на других курсах"]);
+      newSession.polls.electives__17__2["Насколько хорошо преподаватель объясняет материал"] = parseInt(req.body["Насколько хорошо преподаватель объясняет материал"]);
+      newSession.polls.electives__17__2["Насколько хорошо преподаватель организует интерактивную коммуникацию в группе"] = parseInt(req.body["Насколько хорошо преподаватель организует интерактивную коммуникацию в группе"]);
+      newSession.polls.electives__17__2["Что вам больше всего понравилось в этом лекционном курсе"] = req.body["Что вам больше всего понравилось в этом лекционном курсе"];
+      newSession.polls.electives__17__2["Что вам больше всего не понравилось в этом лекционном курсе"] = req.body["Что вам больше всего не понравилось в этом лекционном курсе"];
+      newSession.polls.electives__17__2["Что для вас было самым сложным в этом лекционном курсе"] = req.body["Что для вас было самым сложным в этом лекционном курсе"];
+      newSession.polls.electives__17__2["Как бы вы порекомендовали улучшить этот лекционный курс"] = req.body["Как бы вы порекомендовали улучшить этот лекционный курс"];
+
+      newSession.save(function(err) {
+        if (err)
+          return console.error(err);
+        return;
+      });
+      let mailOptions = {
+        from: '"SAS" <sas@utmn.ru>', // sender address
+        to: 'marat.goya@gmail.com, e.samokhvalova@utmn.ru, a.rusakova@utmn.ru', // list of receivers
+        subject: 'Новый результат опроса по семинарам и лекциям Школы', // Subject line
+        // text: JSON.stringify(req.user), // plain text body
+        html: '<b>' + JSON.stringify(req.session.id) + '<br/><br/>' + JSON.stringify(req.body) + '</b>' // html body
+      };
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          return console.log(error);
+        }
+        console.log('Message %s sent: %s', info.messageId, info.response);
+      });
+    }
+  });
+  req.flash('info', 'Ваш результат принят. Благодарим за участие.');
+  res.render('polls_anonymous', {messages: req.flash('info')})
+});
+});
+
+app.get('/polls/electives', function(req, res) {
+  res.render('electives', {user: req.user})
+});
+
+app.post('/polls/electives', function(req, res) {
+  Session.findOne({
+    'session_id': req.session.id
+  }, function(err, session) {
+    if (err)
+      return done(err);
+ function sessionQuestions(sess){
+
+ }
+
+    if (session) {
+      var now = new Date();
       session.polls["22_23_lectures"]["lecture1"] = req.body["lecture1"];
       session.polls["22_23_lectures"]["lecture2"] = req.body["lecture2"];
       session.polls["22_23_lectures"]["ФИО"] = req.body["ФИО"];
