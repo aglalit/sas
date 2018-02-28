7174252b5bec2bb28d942f972080babc4e029072
Марат <marat.goya@gmail.com>
2018-02-28 16:05:35 +0500

    gi5

--- index.js
+++ index.js
@@ -515,7 +515,6 @@ app.post('/polls/2nd-module-surendra', function(req, res) {
 });
 
 app.get('/polls/global-issues-part-5', function(req, res) {
-
   res.render('global-issues-part-5', {user: req.user})
 });
 
@@ -527,86 +526,55 @@ app.post('/polls/global-issues-part-5', function(req, res) {
       return done(err);
 
     if (session) {
-      var now = new Date();
-      session.polls.GI_part5.time = now.toLocaleString('en-US', {timeZone: 'Asia/Yekaterinburg'});
-      session.polls.GI_part5.LECTURES["Оцените сложность содержания лекций"] = req.body["Оцените сложность содержания лекций"];
-      session.polls.GI_part5.LECTURES["Насколько новым для вас было содержание лекций"] = req.body["Насколько новым для вас было содержание лекций"];
-      session.polls.GI_part5.LECTURES["Насколько изменились ваше мышление, знания и умения под воздействием лекций"] = req.body["Насколько изменились ваше мышление, знания и умения под воздействием лекций"];
-      session.polls.GI_part5.LECTURES["Что вам больше всего понравилось на лекциях?"] = req.body["Что вам больше всего понравилось на лекциях?"];
-      session.polls.GI_part5.LECTURES["Что вам больше всего не понравилось на лекциях?"] = req.body["Что вам больше всего не понравилось на лекциях?"];
-      session.polls.GI_part5.LECTURES["Сколько процентов обязательных текстов по этой части курса вы успевали прочесть"] = req.body["Сколько процентов обязательных текстов по этой части курса вы успевали прочесть"];
-
-      session.polls.GI_part5.SEMINARS["Что именно для вас было непонятным и самым сложным в установке на групповую работу?"] = req.body["Что именно для вас было непонятным и самым сложным в установке на групповую работу?"];
-      session.polls.GI_part5.SEMINARS["Оцените, насколько понятной для вас была установка на групповую работу"] = req.body["Оцените, насколько понятной для вас была установка на групповую работу"];
-      session.polls.GI_part5.SEMINARS["Укажите имя и фамилию куратора, организовывавшего групповую работу"] = req.body["Укажите имя и фамилию куратора, организовывавшего групповую работу"];
-      session.polls.GI_part5.SEMINARS["Оцените, помог ли дополнительный такт (предзащита) в продвижении работы вашей группы?"] = req.body["Оцените, помог ли дополнительный такт (предзащита) в продвижении работы вашей группы?"];
-      session.polls.GI_part5.SEMINARS["Оцените эффективность формата самостоятельной групповой работы (без куратора)"] = req.body["Оцените эффективность формата самостоятельной групповой работы (без куратора)"];
-      session.polls.GI_part5.SEMINARS["Что для вас было самым интересным в групповой работе?"] = req.body["Что для вас было самым интересным в групповой работе?"];
-      session.polls.GI_part5.SEMINARS["Что для вас было самым интересным на пленаре?"] = req.body["Что для вас было самым интересным на пленаре?"];
-      session.polls.GI_part5.SEMINARS["Как бы вы порекомендовали улучшить этот курс"] = req.body["Как бы вы порекомендовали улучшить этот курс"];
-
-      session.save(function(err, session) {
-        if (err)
-          return console.error(err);
-        }
-      );
-      let mailOptions = {
-        from: '"SAS" <sas@utmn.ru>', // sender address
-        to: 'marat.goya@gmail.com, e.samokhvalova@utmn.ru, a.rusakova@utmn.ru', // list of receivers
-        subject: 'Новый результат опроса по семинарам и лекциям Школы', // Subject line
-        // text: JSON.stringify(req.user), // plain text body
-        html: '<b>' + JSON.stringify(req.session.id) + '<br/><br/>' + JSON.stringify(req.body) + '</b>' // html body
-      };
-      transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-          return console.log(error);
-        }
-        console.log('Message %s sent: %s', info.messageId, info.response);
-      });
+      parseSession (session, req, transporter);
     } else {
       var newSession = new Session();
-      var now = new Date();
-      newSession.session_id = req.session.id;
-      newSession.polls.GI_part5.time = now.toLocaleString('en-US', {timeZone: 'Asia/Yekaterinburg'});
-      newSession.polls.GI_part5.LECTURES["Оцените сложность содержания лекций"] = parseInt(req.body["Оцените сложность содержания лекций"]);
-      newSession.polls.GI_part5.LECTURES["Насколько новым для вас было содержание лекций"] = parseInt(req.body["Насколько новым для вас было содержание лекций"]);
-      newSession.polls.GI_part5.LECTURES["Насколько изменились ваше мышление, знания и умения под воздействием лекций"] = parseInt(req.body["Насколько изменились ваше мышление, знания и умения под воздействием лекций"]);
-      newSession.polls.GI_part5.LECTURES["Что вам больше всего понравилось на лекциях?"] = req.body["Что вам больше всего понравилось на лекциях?"];
-      newSession.polls.GI_part5.LECTURES["Что вам больше всего не понравилось на лекциях?"] = req.body["Что вам больше всего не понравилось на лекциях?"];
-
-      newSession.polls.GI_part5.SEMINARS["Что именно для вас было непонятным и самым сложным в установке на групповую работу?"] = req.body["Что именно для вас было непонятным и самым сложным в установке на групповую работу?"];
-      newSession.polls.GI_part5.SEMINARS["Оцените, насколько понятной для вас была установка на групповую работу"] = req.body["Оцените, насколько понятной для вас была установка на групповую работу"];
-      newSession.polls.GI_part5.SEMINARS["Укажите имя и фамилию куратора, организовывавшего групповую работу"] = req.body["Укажите имя и фамилию куратора, организовывавшего групповую работу"];
-      newSession.polls.GI_part5.SEMINARS["Насколько хорошо куратор организует интерактивную коммуникацию в группе?"] = parseInt(req.body["Насколько хорошо куратор организует интерактивную коммуникацию в группе?"]);
-      newSession.polls.GI_part5.SEMINARS["Сколько раз в течение групповой работы вы (в среднем) высказывались вслух?"] = parseInt(req.body["Сколько раз в течение групповой работы вы (в среднем) высказывались вслух?"]);
-      newSession.polls.GI_part5.SEMINARS["Сколько процентов обязательных текстов по этой части курса вы успевали прочесть"] = parseInt(req.body["Сколько процентов обязательных текстов по этой части курса вы успевали прочесть"]);
-      newSession.polls.GI_part5.SEMINARS["Что для вас было самым сложным в групповой работе?"] = req.body["Что для вас было самым сложным в групповой работе?"];
-      newSession.polls.GI_part5.SEMINARS["Что для вас было самым сложным на пленаре?"] = req.body["Что для вас было самым сложным на пленаре?"];
-      newSession.polls.GI_part5.SEMINARS["Как бы вы порекомендовали улучшить этот курс"] = req.body["Как бы вы порекомендовали улучшить этот курс"];
-
-      newSession.save(function(err) {
-        if (err)
-          return console.error(err);
-        return;
-      });
-      let mailOptions = {
-        from: '"SAS" <sas@utmn.ru>', // sender address
-        to: 'marat.goya@gmail.com, e.samokhvalova@utmn.ru, a.rusakova@utmn.ru', // list of receivers
-        subject: 'Новый результат опроса по семинарам и лекциям Школы', // Subject line
-        // text: JSON.stringify(req.user), // plain text body
-        html: '<b>' + JSON.stringify(req.session.id) + '<br/><br/>' + JSON.stringify(req.body) + '</b>' // html body
-      };
-      transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-          return console.log(error);
-        }
-        console.log('Message %s sent: %s', info.messageId, info.response);
-      });
+      parseSession (newSession, req, transporter);
     }
   });
   req.flash('info', 'Ваш результат принят. Благодарим за участие.');
   res.render('polls_anonymous', {messages: req.flash('info')})
 });
+
+function parseSession (sess, req, transporter){
+  var now = new Date();
+  sess.session_id = req.session.id;
+  sess.polls.GI_part5.time = now.toLocaleString('en-US', {timeZone: 'Asia/Yekaterinburg'});
+  sess.polls.GI_part5.LECTURES["Оцените сложность содержания лекций"] = req.body["Оцените сложность содержания лекций"];
+  sess.polls.GI_part5.LECTURES["Насколько новым для вас было содержание лекций"] = req.body["Насколько новым для вас было содержание лекций"];
+  sess.polls.GI_part5.LECTURES["Насколько изменились ваше мышление, знания и умения под воздействием лекций"] = req.body["Насколько изменились ваше мышление, знания и умения под воздействием лекций"];
+  sess.polls.GI_part5.LECTURES["Что вам больше всего понравилось на лекциях?"] = req.body["Что вам больше всего понравилось на лекциях?"];
+  sess.polls.GI_part5.LECTURES["Что вам больше всего не понравилось на лекциях?"] = req.body["Что вам больше всего не понравилось на лекциях?"];
+  sess.polls.GI_part5.LECTURES["Сколько процентов обязательных текстов по этой части курса вы успевали прочесть"] = req.body["Сколько процентов обязательных текстов по этой части курса вы успевали прочесть"];
+
+  sess.polls.GI_part5.SEMINARS["Что именно для вас было непонятным и самым сложным в установке на групповую работу?"] = req.body["Что именно для вас было непонятным и самым сложным в установке на групповую работу?"];
+  sess.polls.GI_part5.SEMINARS["Оцените, насколько понятной для вас была установка на групповую работу"] = req.body["Оцените, насколько понятной для вас была установка на групповую работу"];
+  sess.polls.GI_part5.SEMINARS["Укажите имя и фамилию куратора, организовывавшего групповую работу"] = req.body["Укажите имя и фамилию куратора, организовывавшего групповую работу"];
+  sess.polls.GI_part5.SEMINARS["Оцените, помог ли дополнительный такт (предзащита) в продвижении работы вашей группы?"] = req.body["Оцените, помог ли дополнительный такт (предзащита) в продвижении работы вашей группы?"];
+  sess.polls.GI_part5.SEMINARS["Оцените эффективность формата самостоятельной групповой работы (без куратора)"] = req.body["Оцените эффективность формата самостоятельной групповой работы (без куратора)"];
+  sess.polls.GI_part5.SEMINARS["Что для вас было самым интересным в групповой работе?"] = req.body["Что для вас было самым интересным в групповой работе?"];
+  sess.polls.GI_part5.SEMINARS["Что для вас было самым интересным на пленаре?"] = req.body["Что для вас было самым интересным на пленаре?"];
+  sess.polls.GI_part5.SEMINARS["Как бы вы порекомендовали улучшить этот курс"] = req.body["Как бы вы порекомендовали улучшить этот курс"];
+
+  sess.save(function(err) {
+    if (err)
+      return console.error(err);
+    return;
+  });
+  let mailOptions = {
+    from: '"SAS" <sas@utmn.ru>', // sender address
+    to: 'marat.goya@gmail.com, e.samokhvalova@utmn.ru, a.rusakova@utmn.ru', // list of receivers
+    subject: 'Новый результат опроса по семинарам и лекциям Школы', // Subject line
+    // text: JSON.stringify(req.user), // plain text body
+    html: '<b>' + JSON.stringify(req.session.id) + '<br/><br/>' + JSON.stringify(req.body) + '</b>' // html body
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Message %s sent: %s', info.messageId, info.response);
+  });
+}
 
 
 app.post('/polls-ai-metaphor', function(req, res) {
---
+++ server/gi_part5.js
@@ -0,0 +1,62 @@
+app.get('/polls/global-issues-part-5', function(req, res) {
+
+  res.render('global-issues-part-5', {user: req.user})
+});
+
+app.post('/polls/global-issues-part-5', function(req, res) {
+  Session.findOne({
+    'session_id': req.session.id
+  }, function(err, session) {
+    if (err)
+      return done(err);
+
+    if (session) {
+      parseSession (session, req, transporter);
+    } else {
+      var newSession = new Session();
+      parseSession (newSession, req, transporter);
+    }
+  });
+  req.flash('info', 'Ваш результат принят. Благодарим за участие.');
+  res.render('polls_anonymous', {messages: req.flash('info')})
+});
+
+function parseSession (sess, req, transporter){
+  var now = new Date();
+  sess.session_id = req.session.id;
+  sess.polls.GI_part5.time = now.toLocaleString('en-US', {timeZone: 'Asia/Yekaterinburg'});
+  sess.polls.GI_part5.LECTURES["Оцените сложность содержания лекций"] = req.body["Оцените сложность содержания лекций"];
+  sess.polls.GI_part5.LECTURES["Насколько новым для вас было содержание лекций"] = req.body["Насколько новым для вас было содержание лекций"];
+  sess.polls.GI_part5.LECTURES["Насколько изменились ваше мышление, знания и умения под воздействием лекций"] = req.body["Насколько изменились ваше мышление, знания и умения под воздействием лекций"];
+  sess.polls.GI_part5.LECTURES["Что вам больше всего понравилось на лекциях?"] = req.body["Что вам больше всего понравилось на лекциях?"];
+  sess.polls.GI_part5.LECTURES["Что вам больше всего не понравилось на лекциях?"] = req.body["Что вам больше всего не понравилось на лекциях?"];
+  sess.polls.GI_part5.LECTURES["Сколько процентов обязательных текстов по этой части курса вы успевали прочесть"] = req.body["Сколько процентов обязательных текстов по этой части курса вы успевали прочесть"];
+
+  sess.polls.GI_part5.SEMINARS["Что именно для вас было непонятным и самым сложным в установке на групповую работу?"] = req.body["Что именно для вас было непонятным и самым сложным в установке на групповую работу?"];
+  sess.polls.GI_part5.SEMINARS["Оцените, насколько понятной для вас была установка на групповую работу"] = req.body["Оцените, насколько понятной для вас была установка на групповую работу"];
+  sess.polls.GI_part5.SEMINARS["Укажите имя и фамилию куратора, организовывавшего групповую работу"] = req.body["Укажите имя и фамилию куратора, организовывавшего групповую работу"];
+  sess.polls.GI_part5.SEMINARS["Оцените, помог ли дополнительный такт (предзащита) в продвижении работы вашей группы?"] = req.body["Оцените, помог ли дополнительный такт (предзащита) в продвижении работы вашей группы?"];
+  sess.polls.GI_part5.SEMINARS["Оцените эффективность формата самостоятельной групповой работы (без куратора)"] = req.body["Оцените эффективность формата самостоятельной групповой работы (без куратора)"];
+  sess.polls.GI_part5.SEMINARS["Что для вас было самым интересным в групповой работе?"] = req.body["Что для вас было самым интересным в групповой работе?"];
+  sess.polls.GI_part5.SEMINARS["Что для вас было самым интересным на пленаре?"] = req.body["Что для вас было самым интересным на пленаре?"];
+  sess.polls.GI_part5.SEMINARS["Как бы вы порекомендовали улучшить этот курс"] = req.body["Как бы вы порекомендовали улучшить этот курс"];
+
+  sess.save(function(err) {
+    if (err)
+      return console.error(err);
+    return;
+  });
+  let mailOptions = {
+    from: '"SAS" <sas@utmn.ru>', // sender address
+    to: 'marat.goya@gmail.com, e.samokhvalova@utmn.ru, a.rusakova@utmn.ru', // list of receivers
+    subject: 'Новый результат опроса по семинарам и лекциям Школы', // Subject line
+    // text: JSON.stringify(req.user), // plain text body
+    html: '<b>' + JSON.stringify(req.session.id) + '<br/><br/>' + JSON.stringify(req.body) + '</b>' // html body
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Message %s sent: %s', info.messageId, info.response);
+  });
+}
