26475d48df261bdc71c3263373e72c7493b2993a
Марат <marat.goya@gmail.com>
2018-03-21 11:27:56 +0500

    gi5

--- index.js
+++ index.js
@@ -83,6 +83,89 @@ app.use(compression());
 app.use('/', index);
 app.use(subdomain('advanced', router));
 
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email']
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google', {
+  successRedirect: '/polls/3rd-module-electives',
+  failureRedirect: '/login'
+}));
+// passport.authenticate('google'),
+// function(req, res) {
+//   res.render('polls_ai_metaphor',{user:req.user})
+// });
+
+app.get('/polls', isLoggedIn, function(req, res) {
+  res.render('polls', {user: req.user})
+});
+
+app.get('/polls-anonymous', function(req, res) {
+  res.render('polls_anonymous', {messages: req.flash('info')})
+});
+
+require('./server/gi_part6.js')(app, Session, transporter);
+require('./server/feminism.js')(app, Session, transporter);
+// require('./server/4th-module-electives.js')(app, Session, transporter, isLoggedIn);
+
+app.get('/polls/4th-module-electives', isLoggedIn, function(req, res) {
+  res.render('4th-module-electives', {user: req.user})
+});
+
+app.post('/polls/4th-module-electives', function(req, res) {
+  User.findOne({
+    '_id': req.user._id
+  }, function(err, user) {
+    if (err)
+      return done(err);
+
+    if (user) {
+      user.polls.ELECTIVES["3module__electives"].elective1 = req.body["elective1"];
+      user.polls.ELECTIVES["3module__electives"].elective2 = req.body["elective2"];
+      user.polls.ELECTIVES["3module__electives"].elective3 = req.body["elective3"];
+
+      var now = new Date();
+      user.polls.ELECTIVES["3module__electives"].time = now.toLocaleString('en-US', {timeZone: 'Asia/Yekaterinburg'});
+
+      user.save(function(err, user) {
+        if (err)
+          return console.error(err);
+        }
+      );
+    } else {
+      console.log('There isn\'t such user in the database');
+    }
+  });
+  req.flash('info', `Ваш результат принят (1-й приоритет — ${req.body["elective1"]}; 2-й приоритет — ${req.body["elective2"]}; 3-й приоритет — ${req.body["elective3"]}). Благодарим за участие.`);
+  let mailOptions = {
+    from: '"SAS" <sas@utmn.ru>', // sender address
+    to: 'marat.goya@gmail.com, e.samokhvalova@utmn.ru, a.rusakova@utmn.ru', // list of receivers
+    subject: 'Новый результат опроса по семинарам и лекциям Школы', // Subject line
+    // text: JSON.stringify(req.user), // plain text body
+    html: '<b>' + JSON.stringify(req.user.google.email) + '<br/><br/>' + JSON.stringify(req.body) + '</b>' // html body
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Message %s sent: %s', info.messageId, info.response);
+  });
+  res.render('polls', {
+    user: req.user,
+    messages: req.flash('info')
+  });
+});
+
+
+app.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/polls');
+});
+
 function isLoggedIn(req, res, next) {
 
   // if user is authenticated in the session, carry on
@@ -103,41 +186,6 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
-
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
-
-app.get('/auth/google', passport.authenticate('google', {
-  scope: ['profile', 'email']
-}));
-
-app.get('/auth/google/callback', passport.authenticate('google', {
-  successRedirect: '/polls/4th-module-electives',
-  failureRedirect: '/login'
-}));
-// passport.authenticate('google'),
-// function(req, res) {
-//   res.render('polls_ai_metaphor',{user:req.user})
-// });
-
-app.get('/polls', isLoggedIn, function(req, res) {
-  res.render('polls', {user: req.user})
-});
-
-app.get('/polls-anonymous', function(req, res) {
-  res.render('polls_anonymous', {messages: req.flash('info')})
-});
-
-
-app.get('/logout', function(req, res) {
-  req.logout();
-  res.redirect('/polls');
-});
-
-require('./server/gi_part6.js')(app, Session, transporter);
-require('./server/feminism.js')(app, Session, transporter);
-require('./server/4th-module-electives.js')(app, Session, transporter, isLoggedIn);
 
 // error handler
 app.use(function(err, req, res, next) {
