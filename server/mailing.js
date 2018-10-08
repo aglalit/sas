f2221f010ff7288500d2337314f95d13c52bdd27
Марат Аглиулин <marat@Marats-MacBook-Pro-3.local>
2018-10-08 16:33:22 +0500

    mailing

--- server/mailing.js
+++ server/mailing.js
@@ -32,7 +32,7 @@ app.post('/office/mailing', function(req, res) {
     let mailOptions = {
       from: '"SAS" <a.bunkova@utmn.ru>', // sender address
       to: addresses[i], // list of receivers
-      subject: req.body.letter_topic, // Subject line
+      subject: 'Вами отправлены следующие письма:', // Subject line
       // text: JSON.stringify(req.user), // plain text body
       html: '<p>' + letter + '</p>' // html body
     };
