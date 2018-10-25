eaa412cc7aa6e0cae4120dda0e55a92222723b18
Марат Аглиулин <marat@Marats-MacBook-Pro-3.local>
2018-10-25 15:27:53 +0300

    art

---
+++ server/ma_2018_fedorova.js
@@ -0,0 +1,54 @@
+module.exports = function(app, Session, transporter){
+
+app.get('/polls/ma-2018-fedorova', function(req, res) {
+  res.render('ma-2018-fedorova', {user: req.user})
+});
+
+app.post('/polls/ma-2018-fedorova', function(req, res) {
+  Session.findOne({
+    'session_id': req.session.id
+  }, function(err, session) {
+    if (err)
+      return done(err);
+
+    if (session) {
+      parseSession (session, req, transporter);
+    } else {
+      var newSession = new Session();
+      parseSession (newSession, req, transporter);
+    }
+  });
+  req.flash('info', 'Ваш результат принят. Благодарим за участие.');
+  res.render('polls_anonymous', {messages: req.flash('info')})
+});
+
+function parseSession (sess, req, transporter){
+  var now = new Date();
+  sess.session_id = req.session.id;
+  sess.polls.ma_2018_fedorova.time = now.toLocaleString('en-US', {timeZone: 'Asia/Yekaterinburg'});
+  var keyNames = Object.keys(req.body);
+  keyNames.forEach((el)=>{
+    console.log(req.body[el]);
+
+      sess.polls.ma_2018_fedorova[el] = req.body[el];
+});
+  sess.save(function(err) {
+    if (err)
+      return console.error(err);
+    return;
+  });
+  let mailOptions = {
+    from: '"SAS" <sas@utmn.ru>', // sender address
+    to: 'marat.goya@gmail.com, e.samokhvalova@utmn.ru', // list of receivers
+    subject: 'Новый результат опроса по семинарам и лекциям Школы', // Subject line
+    // text: JSON.stringify(req.user), // plain text body
+    html: '<b>' + JSON.stringify(req.session.id) + '<br/><br/>' + JSON.stringify(req.body) + '</b>' // html body
+  };
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Message %s sent: %s', info.messageId, info.response);
+  });
+}
+}
