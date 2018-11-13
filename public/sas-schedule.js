5e231a2e40b0910fa72bc8ad95ce741cd9b43db2
Марат Аглиулин <marat@Marats-MacBook-Pro-3.local>
2018-11-13 18:03:25 +0500

    schedule

--- public/sas-schedule.js
+++ public/sas-schedule.js
@@ -69,4 +69,10 @@ function generateSchedule(sheet){
     // document.querySelectorAll('.row')[k].appendChild(cell);
   // }
 }
-setInterval(function(){ generateSchedule(sheetGlobal); console.log("refreshed"); }, 10000);
+setInterval(function(){
+  let rows = document.querySelectorAll('.row');
+  for (let t=0;t<rows.length;t++){
+    document.querySelector(".table").deleteRow(0);
+  }
+  generateSchedule(sheetGlobal);
+  console.log("refreshed"); }, 10000);
