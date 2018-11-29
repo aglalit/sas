374a80e02514ba5c4d549b135fb03a4af9d34d05
Марат Аглиулин <marat@Marats-MacBook-Pro-3.local>
2018-11-29 16:07:51 +0500

    heatmap

--- public/sas-schedule.bundle.js
+++ public/sas-schedule.bundle.js
@@ -4918,12 +4918,7 @@ function generateSchedule(sheet){
     row.appendChild(cell);
     if(!cell.innerHTML){row.remove()}
   }
-  if (moment().format('ss')%2 == 0){
-    document.querySelector('.date').innerHTML = moment().format('MMMM D, H:mm');
-  }
-  else {
-    document.querySelector('.date').innerHTML = moment().format('MMMM D, H<b>:</b>mm');
-  }
+  document.querySelector('.date').innerHTML = moment().format('MMMM D, H:mm');
 
 
 
@@ -4946,6 +4941,6 @@ function generateSchedule(sheet){
 }
 setInterval(function(){
   GetSheetDone.labeledCols('120_7j9FsFxBkoG2W0aX0d4wdgKP2r2RK52wNMq52frc').then(sheet => generateSchedule(sheet));
-  console.log("refreshed"); }, 1000);
+  console.log("refreshed"); }, 2000);
 
 },{"get-sheet-done":2,"moment":3}]},{},[4]);
