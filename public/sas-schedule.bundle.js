0982392adbfc370b8634acfaaf23fb545295681a
Марат Аглиулин <marat@Marats-MacBook-Pro-3.local>
2019-02-15 11:52:56 +0500

    s

--- public/sas-schedule.bundle.js
+++ public/sas-schedule.bundle.js
@@ -4956,23 +4956,23 @@ setInterval(function(){
   GetSheetDone.labeledCols('120_7j9FsFxBkoG2W0aX0d4wdgKP2r2RK52wNMq52frc').then(sheet => generateSchedule(sheet));
   console.log("refreshed"); }, 2000);
 
-var justHidden = false;
-var j;
-document.getElementsByTagName('body')[0].style.cursor = 'none';
-function hide() {
-  document.getElementsByTagName('body')[0].style.cursor = 'none';
-  justHidden = true;
-  setTimeout(function() {
-    justHidden = false;
-  }, 500);
-};
-document.addEventListener("mousemove", function() {
-  if (!justHidden) {
-    justHidden = false;
-    clearTimeout(j);
-      document.getElementsByTagName('body')[0].style.cursor = 'default';
-    j = setTimeout(hide, 1000);
-  }
-});
+// var justHidden = false;
+// var j;
+// document.getElementsByTagName('body')[0].style.cursor = 'none';
+// function hide() {
+//   document.getElementsByTagName('body')[0].style.cursor = 'none';
+//   justHidden = true;
+//   setTimeout(function() {
+//     justHidden = false;
+//   }, 500);
+// };
+// document.addEventListener("mousemove", function() {
+//   if (!justHidden) {
+//     justHidden = false;
+//     clearTimeout(j);
+//       document.getElementsByTagName('body')[0].style.cursor = 'default';
+//     j = setTimeout(hide, 1000);
+//   }
+// });
 
 },{"get-sheet-done":2,"moment":3}]},{},[4]);
