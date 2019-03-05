e649194a4dcfad41f4451d025d8c3fa9df8d8220
Марат Аглиулин <marat@Marats-MacBook-Pro-3.local>
2019-03-05 12:16:43 +0500

    s

--- public/sas-schedule.js
+++ public/sas-schedule.js
@@ -134,7 +134,7 @@ function generateSchedule(sheet){
 }
 setInterval(function(){
   GetSheetDone.labeledCols('120_7j9FsFxBkoG2W0aX0d4wdgKP2r2RK52wNMq52frc').then(sheet => generateSchedule(sheet));
-  console.log("refreshed"); }, 3000);
+  console.log("refreshed"); }, 2000);
 
 // var justHidden = false;
 // var j;
