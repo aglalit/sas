d3c6b934be1ffd357e7b5c05a6e15845c7667468
Марат Аглиулин <marat@Marats-MacBook-Pro.local>
2019-03-29 15:32:24 +0500

    s

--- server/ba_2019_year2_module7_gb.js
+++ server/ba_2019_year2_module7_gb.js
@@ -39,7 +39,7 @@ function parseSession (sess, req, transporter){
   let mailOptions = {
     from: '"SAS" <sas@utmn.ru>', // sender address
     to: 'marat.goya@gmail.com,s.makhmudova@utmn.ru', // list of receivers
-    subject: 'GB — feedback', // Subject line
+    subject: 'GB Literature — feedback', // Subject line
     // text: JSON.stringify(req.user), // plain text body
     html: emailBody.toString() // html body
   };
