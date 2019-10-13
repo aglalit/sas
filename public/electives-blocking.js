8eb76a5aa255895c1da6810e5577df0f3a5ba1b9
Марат Аглиулин <marat@Marats-MacBook-Pro.local>
2019-10-13 11:23:20 +0300

    electives

--- public/electives-blocking.js
+++ public/electives-blocking.js
@@ -1,21 +1,24 @@
-var inputs = document.querySelectorAll('input[type="radio"]');
-inputs.forEach(function(input){
-  input.addEventListener('click', function(){
-    var inp = input;
-    inputs.forEach(function(i){
-      if (inp.value === i.value){
-        i.disabled = true;
-      }
-      else {
-        i.disabled = false;
+var inputGroups = document.querySelectorAll('.input-group');
+inputGroups.forEach(function(group){
+  var inputs = group.querySelectorAll('input[type="radio"]');
+  inputs.forEach(function(input){
+    input.addEventListener('click', function(){
+      var inp = input;
+      inputs.forEach(function(i){
+        if (inp.value === i.value){
+          i.disabled = true;
         }
+        else {
+          i.disabled = false;
+          }
+      });
+      inputs.forEach(function(i){
+      if (i.checked) {
+        var checked = group.querySelectorAll('input[value="'+i.value+'"]');
+        checked.forEach(function(el){el.disabled = true;});
+        i.disabled = false;
+      }
     });
-    inputs.forEach(function(i){
-    if (i.checked) {
-      var checked = document.querySelectorAll('input[value="'+i.value+'"]');
-      checked.forEach(function(el){el.disabled = true;});
-      i.disabled = false;
-    }
+    });
   });
-  });
-});
+})
