a85f259e32745ec8c470ce915ad5a335d075bb95
Марат Аглиулин <marat@Marats-MacBook-Pro.local>
2019-11-12 18:30:38 +0500

    reg-list

--- server/registration-list.js
+++ server/registration-list.js
@@ -1,7 +1,7 @@
 module.exports = function(app, Session, transporter, logger){
 
 app.get('/registration-list', function(req, res) {
-  Session.find({'polls.registration': {$exists : true}}).select('polls.registration -_id').exec(function (err, docs){
+  Session.find({'polls.registration': {$exists : true}}).select('polls.registration -_id').limit(200).exec(function (err, docs){
     if (err) {
       res.send(err);
       logger.error(err);
