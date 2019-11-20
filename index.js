58d070c885f62485994fd4cc9a3d18869874fbf6
Марат Аглиулин <marat@Marats-MacBook-Pro.local>
2019-11-20 10:52:49 +0500

    gi_topics_2020

--- index.js
+++ index.js
@@ -242,6 +242,8 @@ require('./server/gi_topics_2020.js')(app, Session, transporter, isLoggedIn, Use
 require('./server/registration-data.js')(app, Session, transporter, isLoggedIn, logger);
 require('./server/feedback.js')(app, Session, transporter, logger);
 require('./server/feedback-collector.js')(app, Session, transporter, officeTransporter, isLoggedIn, User, logger);
+require('./server/feedback-collector-anonymous.js')(app, Session, transporter, officeTransporter, logger);
+
 require('./server/db_export.js')(app, Session, User);
 
 
---
+++ server/feedback-collector-anonymous.js
@@ -0,0 +1,89 @@
+module.exports = function(app, Session, transporter, officeTransporter, logger) {
+
+  var name = 'anonymous';
+  var email = 'anonymous@gmail.com';
+
+  app.get('/polls/ba-2019-year1-module1-english', function(req, res) {
+    res.render('ba-2019-year1-module1-english')
+  });
+
+  app.post('/polls/feedback-collector-anonymous', function(req, res) {
+
+      Session.findOne({
+        'session_id': req.session.id
+      }, function(err, session) {
+        if (err)
+          logger.error(err);
+          console.log(err);
+
+        if (session) {
+          parseSession(session, req, transporter);
+        } else {
+          var newSession = new Session();
+          parseSession(newSession, req, transporter);
+        }
+      });
+
+    req.flash('info', 'Ответ принят. Благодарим за обратную связь ( ͡° ͜ʖ ͡°)');
+    res.render('polls_anonymous', {
+      messages: req.flash('info')
+    })
+
+    function parseSession(sess, req, transporter) {
+      var now = new Date();
+      sess.session_id = req.session.id;
+      //   var keyNames = Object.keys(req.body);
+      //   keyNames.forEach((el)=>{
+      //     console.log(req.body[el]);
+      //
+      //       sess.polls.ba_2018_year2_the_city_as_text[el] = req.body[el];
+      // });
+      console.log(req.body);
+      let emailBody = '';
+      var bodyKeys = Object.keys(req.body);
+      for (let i = 0; i < bodyKeys.length; i++) {
+        emailBody += '<p><b>' + bodyKeys[i] + '</b>: ' + req.body[bodyKeys[i]] + '</p>';
+      }
+      let mailOptions = {
+        from: '"SAS" <sas@utmn.ru>', // sender address
+        to: 'm.agliulin@utmn.ru', // list of receivers
+        subject: 'Feedback', // Subject line
+        // text: JSON.stringify(req.user), // plain text body
+        html: emailBody.toString() // html body
+      };
+      officeTransporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          logger.error(error);
+          logger.error(mailOptions.subject);
+          logger.error(mailOptions.html);
+          return console.log(error);
+        }
+        console.log('Message %s sent: %s', info.messageId, info.response);
+      });
+      mailOptions = {
+        from: '"SAS" <sas@utmn.ru>', // sender address
+        to: 'walerieorlova@gmail.com', // list of receivers
+        // to: 'marat.goya@gmail.com', // list of receivers
+        subject: `${name}: ${email}`, // Subject line
+        // text: JSON.stringify(req.user), // plain text body
+        html:  req.body.subject.replace(/-/g, '_')
+      };
+      // transporter.sendMail(mailOptions, (error, info) => {
+      //   if (error) {
+      //     logger.error(error);
+      //     logger.error(mailOptions.subject);
+      //     logger.error(mailOptions.html);
+      //     return console.log(error);
+      //   }
+      //   console.log('Message %s sent: %s', info.messageId, info.response);
+      // });
+      sess.polls[req.body.subject.replace(/-/g, '_')] = JSON.stringify(req.body);
+      sess.save(function(err) {
+        if (err)
+          logger.error(err);
+          return console.error(err);
+        return;
+      });
+    }
+  });
+}
