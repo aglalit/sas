f0090f232cc79662e812735b482644dbaa673a2d
Марат Аглиулин <marat@Marats-MacBook-Pro.local>
2019-12-03 12:26:48 +0500

    openday

--- server/faculty-research-trips.js
+++ server/faculty-research-trips.js
@@ -52,7 +52,7 @@ function parseSession (sess, req, transporter){
 
   let mailOptions = {
     from: '"SAS" <sas@utmn.ru>', // sender address
-    to: 'm.agliulin@utmn.ru,v.savina@utmn.ru', // list of receivers
+    to: 'm.agliulin@utmn.ru,f.gook@utmn.ru', // list of receivers
     subject: 'SAS — research trips', // Subject line
     // text: JSON.stringify(req.user), // plain text body
     html: emailBody.toString(),
