ec33890439ce3d4405eaa97d0582e04e405e039e
Марат Аглиулин <marat@Marats-MacBook-Pro.local>
2019-12-13 16:42:04 +0500

    2020_1

--- server/electives_2020_1.js
+++ server/electives_2020_1.js
@@ -70,7 +70,7 @@ module.exports = function(app, Session, transporter, isLoggedIn, User){
     else {
       console.log('req.user doesnt exist');
       req.flash('error', `An error occurred, please try again
-      res.render('polls', {
+      res.render('login', {
         messages: req.flash('error')
       });
     }
