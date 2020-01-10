9ffe7c9841554691e11705f25eba826714d1a940
Марат Аглиулин <marat@Marats-MacBook-Pro.local>
2020-01-10 18:26:01 +0500

    RS

--- public/feedback3.js
+++ public/feedback3.js
@@ -328,22 +328,6 @@ function displaySubject(data, subject) {
           });
           avg = (sum / dataNumbersToReduce[Object.keys(dataNumbersToReduce)[i]][key].length).toPrecision(2);
         }
-        data.push({
-          histfunc: 'count',
-          //x: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'],
-          x: dataNumbers[Object.keys(dataNumbers)[i]][key],
-          type: 'histogram',
-          name: Object.keys(dataNumbers)[i],
-          marker: {
-            color: color
-          },
-          xbins: {
-            start: 0.5,
-            end: 10.5,
-            size: 1,
-          },
-
-        })
       }
 
       var layout = {
