ecda382b039d6f3e120e161369b84f0b18440b85
aglalit <marat.goya@gmail.com>
2020-03-04 13:07:43 +0500

    fix

--- index.js
+++ index.js
@@ -220,7 +220,7 @@ require('./server/registration-dobrovidova.js')(app, Session, transporter);
 
 require('./server/open-day-2018.js')(app, Session, transporter);
 require('./server/open-day-2018-voting.js')(app, Session, transporter);
-require('./server/open-day-2019.js')(app, Session, transporter);
+require('./server/open-day-03-2020.js')(app, Session, transporter);
 require('./server/pds-2019.js')(app, Session, transporter);
 require('./server/registration.js')(app, Session, transporter);
 require('./server/poetry.js')(app, Session, transporter);
