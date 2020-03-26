5065ba509c4a5d4e3702a5c50008b3c706882977
aglalit <marat.goya@gmail.com>
2020-03-26 16:56:34 +0500

    'q3_3 tweaks 2 localStorage.clear();'

--- public/default.js
+++ public/default.js
@@ -5,44 +5,48 @@
  * Author: Sarkhan Rajabov
 **/
 
-// (function($){
-//     $.fn.saveStorage = function(){
-//         'use strict';
-//
-//         if(typeof Storage !== "undefined"){
-//             var form = $(this),
-//                 key = $(this).attr('id')+'_saveStorage';
-//
-//             form.find('input,select,textarea').bind('change keyup', function () {
-//                 var serializeForm = form.serializeArray();
-//                 localStorage.setItem(key, JSON.stringify(serializeForm));
-//             });
-//
-//             var data = JSON.parse(localStorage.getItem(key));
-//
-//             $(data).each(function(k,v){
-//                 form.find(':input[name="'+v.name+'"]').not('input[type=radio], input[type=checkbox], input[type=hidden]').val(v.value);
-//
-//                 $(form.find('input[type=radio]')).each(function () {
-//                     if($(this).attr('name') === v.name && $(this).attr('value') === v.value){
-//                         $(this).prop('checked', true)
-//                     }
-//                 });
-//                 $(form.find('input[type=checkbox]')).each(function () {
-//                     if($(this).attr('name') === v.name && $(this).attr('value') === v.value){
-//                         $(this).prop('checked', true)
-//                     }
-//                 });
-//             });
-//
-//             // form.submit(function () {
-//             //     localStorage.removeItem(key);
-//             // })
-//         }
-//         else {
-//             console.error('Sorry! No web storage support.')
-//         }
-//     };
-// })(jQuery);
-//
-// $('#form').saveStorage();
+(function($){
+    $.fn.saveStorage = function(){
+        'use strict';
+
+        if(typeof Storage !== "undefined"){
+            var form = $(this),
+                key = $(this).attr('id')+'_saveStorage';
+
+            form.find('input,select,textarea').bind('change keyup', function () {
+                var serializeForm = form.serializeArray();
+                localStorage.setItem(key, JSON.stringify(serializeForm));
+            });
+
+            var data = JSON.parse(localStorage.getItem(key));
+
+            $(data).each(function(k,v){
+                form.find(':input[name="'+v.name+'"]').not('input[type=radio], input[type=checkbox], input[type=hidden]').val(v.value);
+
+                $(form.find('input[type=radio]')).each(function () {
+                    if($(this).attr('name') === v.name && $(this).attr('value') === v.value){
+                        $(this).prop('checked', true)
+                    }
+                });
+                $(form.find('input[type=checkbox]')).each(function () {
+                    if($(this).attr('name') === v.name && $(this).attr('value') === v.value){
+                        $(this).prop('checked', true)
+                    }
+                });
+            });
+
+            // form.submit(function () {
+            //     localStorage.removeItem(key);
+            // })
+        }
+        else {
+            console.error('Sorry! No web storage support.')
+        }
+    };
+})(jQuery);
+
+$('#form').saveStorage();
+
+$('button[type="submit"]').click(function(){
+    localStorage.clear();
+})
