7794087ff9dd1755265bcca6f8e6178ec0ce3913
aglalit <marat.goya@gmail.com>
2020-11-10 19:24:14 +0500

    feedback q1 20-21

--- public/feedback6.js
+++ public/feedback6.js
@@ -298,6 +298,7 @@ function displaySubject (data, subject) {
   }
   if (subjectUrl === 'all') {
     for (var t in dataNumbers) {
+      console.log(t);
       if (t !== undefined && t !== 'undefined' && t !== 'Anna Syrchina'){
       // var dataNumbersToExport = iterationCopy(dataNumbers[t]);
       //
