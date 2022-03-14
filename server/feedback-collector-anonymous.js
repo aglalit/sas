a1c83f5a6b2e3ab8f5229cd8c8910853c48f56d4
aglalit <marat.goya@gmail.com>
2022-03-14 15:47:09 +0500

    elect 2022 4

--- server/feedback-collector-anonymous.js
+++ server/feedback-collector-anonymous.js
@@ -54,7 +54,7 @@ module.exports = function (app, Session, transporter, officeTransporter, logger)
         }
         console.log('Message %s sent: %s', info.messageId, info.response);
       });
-      sess.polls[req.body.subject.replace(/-/g, '_')] = JSON.stringify(req.body);
+      sess.polls.generic_anonymous = JSON.stringify(req.body);
       sess.save(function (err) {
         if (err) { logger.error(err); }
         return console.error(err);
