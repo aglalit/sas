f001c1acb250b7e245a66d1919636c27cd52623b
aglalit <marat.goya@gmail.com>
2022-04-28 14:40:00 +0500

    elect 2022 4

--- server/registration.js
+++ server/registration.js
@@ -76,7 +76,7 @@ function parseSession (sess, req, transporter){
   }
   let mailOptions = {
     from: '"SAS" <sas@utmn.ru>', // sender address
-    to: 'm.agliulin@utmn.ru,v.savina@utmn.ru', // list of receivers
+    to: 'm.agliulin@utmn.ru', // list of receivers
     subject: 'SAS — Registration', // Subject line
     // text: JSON.stringify(req.user), // plain text body
     html: emailBody.toString() // html body
